db381650ee2904d25be7e7575484f7c3f5f66153
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-11-14 18:43:10 +0300

    Update storage_information_view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,22 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from datacenter.models import get_duration
+from datacenter.models import format_duration
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    all_non_ended_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for non_ended_visit in all_non_ended_visits:
+        duration = get_duration(non_ended_visit.entered_at)
+        non_closed_visits.append(
+            {
+                'who_entered': non_ended_visit.passcard.owner_name,
+                'entered_at': non_ended_visit.entered_at,
+                'duration': format_duration(duration),
+            }
+        )
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
