1bb47cdca9ef439b8885209f83691aec125cd5e2
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-11-15 23:08:56 +0300

    Refactor names of variables

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,18 +5,18 @@ from datacenter.models import format_duration
 
 
 def storage_information_view(request):
-    all_non_ended_visits = Visit.objects.filter(leaved_at=None)
-    non_closed_visits = []
-    for non_ended_visit in all_non_ended_visits:
-        duration = get_duration(non_ended_visit.entered_at)
-        non_closed_visits.append(
+    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    serialized = []
+    for non_closed_visit in non_closed_visits:
+        duration = get_duration(non_closed_visit.entered_at)
+        serialized.append(
             {
-                'who_entered': non_ended_visit.passcard.owner_name,
-                'entered_at': non_ended_visit.entered_at,
+                'who_entered': non_closed_visit.passcard.owner_name,
+                'entered_at': non_closed_visit.entered_at,
                 'duration': format_duration(duration),
             }
         )
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized,
     }
     return render(request, 'storage_information.html', context)
